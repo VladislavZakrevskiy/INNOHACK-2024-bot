d5434e461a2925f9463a07426748f710abef2acb
VolosnjovFA <feodor.a.v.03@gamil.com>
2024-09-28 00:39:39 +0300

    default1

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Fedor\Desktop\VScodeProjects\Projects\bot\.venv
+set VIRTUAL_ENV=C:\VisualStudioProjects\bot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
