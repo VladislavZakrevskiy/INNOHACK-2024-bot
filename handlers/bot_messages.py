fb4effede39437a777b41fe777438001c898cbe6
Fedor <feodor.a.v.03@gmail.com>
2024-09-28 17:45:06 +0300

    full task data for user

--- handlers/bot_messages.py
+++ handlers/bot_messages.py
@@ -16,7 +16,7 @@ PORT = config.port.get_secret_value()
 
 router = Router()
 
-async def list_projects(telegram_id, message):
+async def list_projects(telegram_id: str, message: Message) -> list:
     users_connection = psycopg2.connect(
     host=HOST,
     user=USER,
@@ -40,32 +40,82 @@ async def list_projects(telegram_id, message):
     )
 
     with projects_connection.cursor() as cursor:
+
+        # получение задач
         cursor.execute(
             """SELECT owner_id, create_date, deadline_date, description, title, status_id FROM task WHERE executor_id = %s;""", (user_id,)
         )
         tasks = cursor.fetchall()
+
+        # получение информации о каждой задаче
+
         for task in tasks:
             (task_owner_id, task_create_date, task_deadline_date, task_description, task_title, task_status_id) = task
+            day = task_deadline_date.date().day
+            month = task_deadline_date.date().month
+            hour = task_deadline_date.time().hour
+            minute = task_deadline_date.time().minute
+            day_left = (datetime.datetime.now() - task_deadline_date).days
+
+            #получение информации о статусе задачи
+
+            cursor.execute(
+            """SELECT name, space_id FROM status WHERE id = %s;""", (task_status_id,)
+            )
+            (status_name, space_id) = cursor.fetchone()
+
+            #получение информации о space, в котором лежит задача
+
+            cursor.execute(
+            """SELECT owner_id, description, name, project_id FROM space WHERE id = %s;""", (space_id,)
+            )
+            (space_owner_id, space_description, space_name, project_id) = cursor.fetchone()
+
+            # получение информации о проекте, в котором находится space
+
+            cursor.execute(
+            """SELECT owner_id, description, image, name FROM project WHERE id = %s;""", (project_id,)
+            )
+            (project_owner_id, project_description, project_image, project_name) = cursor.fetchone()
+            
+            #USER DATABASE Получение имен владельцев
             with users_connection.cursor() as users_cursor:
                 users_cursor.execute(
                 """SELECT full_name FROM users WHERE id = %s;""", (task_owner_id,)
                 )
                 task_owner_fullname = users_cursor.fetchone()
-                day = task_deadline_date.date().day
-                month = task_deadline_date.date().month
-                hour = task_deadline_date.time().hour
-                minute = task_deadline_date.time().minute
-                differense = datetime.datetime.now() - task_deadline_date
-                if differense.days == 0:
-                    deadline_info = "⚠️СЕГОДНЯ⚠️"
+                users_cursor.execute(
+                """SELECT full_name FROM users WHERE id = %s;""", (space_owner_id,)
+                )
+                space_owner_fullname = users_cursor.fetchone()
+                users_cursor.execute(
+                """SELECT full_name FROM users WHERE id = %s;""", (project_owner_id,)
+                )
+                project_owner_fullname = users_cursor.fetchone()
+
+                data = (
+                    user_fullname,
+                    ((task_create_date, day, month, hour, minute, day_left), task_title, task_owner_fullname, task_description),
+                    (status_name),
+                    (space_name, space_owner_fullname, space_description),
+                    (project_name, project_image, project_owner_fullname, project_description)
+                    
+                    )
+                if day_left == 0:
+                    deadline_data = "⚠️СЕГОДНЯ⚠️"
                 else:
-                    deadline_info = f"⚠️Осталось дней: {differense.days}⚠️"
-            await message.answer(f"🛠️<b>{task_title}</b>:\n\n📅Дэдлайн: {day}.{month} до {hour}:{minute}  {deadline_info}\n\n💬{task_description}\n🗣️Руководитель: <b>{task_owner_fullname[0]}</b>")
+                    deadline_data = f"⚠️Осталось дней: {day_left}⚠️"
+            await message.answer(f"🛠️<b>{task_title}</b>:\n\n📅Дэдлайн: {day}.{month} до {hour}:{minute}  {deadline_data}\n\n💬{task_description}\n\n🗣️Руководитель: <b>{task_owner_fullname[0]}</b>")
 
     
     projects_connection.close()
     users_connection.close()
     print("[INFO] PostgreSQL connection closed")
+    return data
+
+
+
+
 
 
 @router.message()
@@ -73,7 +123,7 @@ async def main_menu(message: Message, state: FSMContext):
     msg = message.text.lower()
     print(msg)
     if msg == "мои задачи":
-        await list_projects(str(message.from_user.id), message)
+        data = await list_projects(str(message.from_user.id), message)
     elif msg == "сменить пропуск":
         await state.set_state(Start.login)
         await message.answer("Отправьте новый <b>Пропуск</b>")
